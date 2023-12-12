f4219a449b42cc9a5fe35985071ef7bf6ed93218
Manav Sharma <sharma.manav@northeastern.edu>
2023-12-11 20:52:28 -0500

    * chore(lambda_function.py): add start_session function to generate a unique session ID

--- lambda_function.py
+++ lambda_function.py
@@ -10,3 +10,9 @@ table = dynamodb.Table('SessionTable')  # Replace with your DynamoDB table name
 
 # Retrieve the OpenAI API key from environment variables
 openai.api_key = os.environ['OPENAPI_KEY']
+
+def start_session(transcript):
+    # Generate a unique session ID
+    session_id = str(uuid.uuid4())
+    
+    return session_id
