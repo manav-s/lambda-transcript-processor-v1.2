56d1b829feb63fc0b2077f172412e2211be937fd
Manav Sharma <sharma.manav@northeastern.edu>
2023-12-11 21:54:25 -0500

    * fix(lambda_function.py): fix function name from start_session to start_session_handler * feat(lambda_function.py): add ask_question_handler function to handle asking questions * feat(lambda_function.py): add error handling for session not found in ask_question_handler * feat(lambda_function.py): add error handling for invalid path or method in lambda_handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
-import os
 import json
+import os
 import boto3
 import uuid
 import openai
@@ -11,7 +11,7 @@ table = dynamodb.Table('SessionTable')  # Replace with your DynamoDB table name
 # Retrieve the OpenAI API key from environment variables
 openai.api_key = os.environ['OPENAPI_KEY']
 
-def start_session(transcript):
+def start_session_handler(transcript):
     # Generate a unique session ID
     session_id = str(uuid.uuid4())
     
@@ -22,64 +22,59 @@ def start_session(transcript):
         'Context': ''  # Placeholder for any initial context if needed
     })
     
-    return session_id
+    # Return the session ID to the client
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'sessionId': session_id, 'message': 'Session started'})
+    }
 
-def ask_question(transcript, question):
-    # Combine the transcript and the question to form the prompt
-    prompt = f"{transcript}\n\n{question}"
-    
-    # Send the prompt to OpenAI's GPT-3.5-turbo-1106 model
-    gpt_response = openai.Completion.create(
-        engine="text-davinci-003",  # Replace with your chosen model
-        prompt=prompt,
-        max_tokens=1024  # Adjust as needed
-    )
-    
-    # Extract the response text
-    answer = gpt_response.choices[0].text.strip()
-    
-    return answer
+def ask_question_handler(session_id, question):
+    # Retrieve the session data from DynamoDB
+    response = table.get_item(Key={'SessionId': session_id})
+    if 'Item' in response:
+        session_data = response['Item']
+        transcript = session_data['Transcript']
+        
+        # Combine the transcript and the question to form the prompt
+        prompt = f"{transcript}\n\n{question}"
+        
+        # Send the prompt to OpenAI's GPT-3.5-turbo model
+        gpt_response = openai.Completion.create(
+            engine="text-davinci-003",  # Replace with your chosen model
+            prompt=prompt,
+            max_tokens=1024  # Adjust as needed
+        )
+        
+        # Extract the response text
+        answer = gpt_response.choices[0].text.strip()
+        
+        # Return the answer to the client
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'answer': answer})
+        }
+    else:
+        # Handle session not found
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'error': 'Session not found'})
+        }
 
 def lambda_handler(event, context):
-    # Extract the transcript and question from the request
-    transcript = event.get('transcript')
-    question = event.get('question')
+    path = event.get('rawPath', '')  # Get the path from the event object
+    method = event.get('requestContext', {}).get('http', {}).get('method', 'GET')
     
-    # If a transcript is provided, start the session and get the session ID
-    if transcript:
-        session_id = start_session(transcript)
-        # If a question is provided along with the transcript, get an answer
-        if question:
-            answer = ask_question(transcript, question)
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'sessionId': session_id, 'answer': answer})
-            }
-        else:
-            # If no question is provided, just return the session ID
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'sessionId': session_id})
-            }
-    # If only a question is provided, fetch the transcript from the session and return an answer
-    elif question and 'sessionId' in event:
-        session_id = event['sessionId']
-        # Retrieve the session data from DynamoDB
-        response = table.get_item(Key={'SessionId': session_id})
-        if 'Item' in response:
-            session_data = response['Item']
-            answer = ask_question(session_data['Transcript'], question)
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'answer': answer})
-            }
-        else:
-            return {
-                'statusCode': 404,
-                'body': json.dumps({'error': 'Session not found'})
-            }
-    else:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'No transcript or question provided'})
-        }
+    if method == 'POST':
+        body = json.loads(event.get('body', '{}'))
+    
+        if path == '/start-session' and 'transcript' in body:
+            return start_session_handler(body['transcript'])
+        
+        elif path == '/ask-question' and 'sessionId' in body and 'question' in body:
+            return ask_question_handler(body['sessionId'], body['question'])
+
+    # Default response for invalid path or method
+    return {
+        'statusCode': 400,
+        'body': json.dumps({'error': 'Invalid path or method'})
+    }
