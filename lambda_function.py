3e7d08fda0b888ccbe014e9980080af26f816a73
Manav Sharma <sharma.manav@northeastern.edu>
2023-12-11 22:12:12 -0500

    * feat(lambda_function.py): add support for starting a session and asking a question * fix(lambda_function.py): fix typo in OpenAI API key environment variable name

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,15 @@
-# Other imports and initialization code remains the same
+import json
+import os
+import boto3
+import uuid
+import openai
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('SessionTable')  # Replace with your DynamoDB table name
+
+# Retrieve the OpenAI API key from environment variables
+openai.api_key = os.environ['OPENAPI_KEY']
 
 def delete_session_handler(session_id):
     # Delete the session data from DynamoDB
@@ -9,6 +20,55 @@ def delete_session_handler(session_id):
         'statusCode': 200,
         'body': json.dumps({'message': f'Session {session_id} deleted successfully'})
     }
+
+def start_session_handler(transcript):
+    # Generate a unique session ID
+    session_id = str(uuid.uuid4())
+    
+    # Store the session data in DynamoDB
+    table.put_item(Item={
+        'SessionId': session_id,
+        'Transcript': transcript,
+        'Context': ''  # Placeholder for any initial context if needed
+    })
+    
+    # Return the session ID to the client
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'sessionId': session_id, 'message': 'Session started'})
+    }
+
+def ask_question_handler(session_id, question):
+    # Retrieve the session data from DynamoDB
+    response = table.get_item(Key={'SessionId': session_id})
+    if 'Item' in response:
+        session_data = response['Item']
+        transcript = session_data['Transcript']
+        
+        # Combine the transcript and the question to form the prompt
+        prompt = f"{transcript}\n\n{question}"
+        
+        # Send the prompt to OpenAI's GPT-3.5-turbo model
+        gpt_response = openai.Completion.create(
+            engine="text-davinci-003",  # Replace with your chosen model
+            prompt=prompt,
+            max_tokens=1024  # Adjust as needed
+        )
+        
+        # Extract the response text
+        answer = gpt_response.choices[0].text.strip()
+        
+        # Return the answer to the client
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'answer': answer})
+        }
+    else:
+        # Handle session not found
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'error': 'Session not found'})
+        }
 
 def lambda_handler(event, context):
     path = event.get('rawPath', '')  # Get the path from the event object
